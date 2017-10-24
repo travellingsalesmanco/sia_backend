0a77bf11cb2eff0c18337bf41b812b25c1e85b89
johnphua <phuajohn0@gmail.com>
2017-10-24 20:07:51 +0800

    WIP Openshift database link

--- sia_backend/database.py
+++ sia_backend/database.py
@@ -11,45 +11,34 @@ engines = {
 
 
 def config():
+    import os
+    DJ_PROJECT_DIR = os.path.dirname(__file__)
+    BASE_DIR = os.path.dirname(DJ_PROJECT_DIR)
+    WSGI_DIR = os.path.dirname(BASE_DIR)
+    REPO_DIR = os.path.dirname(WSGI_DIR)
+    DATA_DIR = os.environ.get('OPENSHIFT_DATA_DIR', BASE_DIR)
 
     # a setting to determine whether we are running on OpenShift
     ON_OPENSHIFT = False
     if os.environ.has_key('OPENSHIFT_REPO_DIR'):
         ON_OPENSHIFT = True
-    if os.environ.has_key('OPENSHIFT_DB_NAME'):
-        DB_NAME = os.environ['OPENSHIFT_DB_NAME']
-    if os.environ.has_key('OPENSHIFT_DB_USERNAME'):
-        DB_USER = os.environ['OPENSHIFT_DB_USERNAME']
-    if os.environ.has_key('OPENSHIFT_DB_PASSWORD'):
-        DB_PASSWD = os.environ['OPENSHIFT_DB_PASSWORD']
-    if os.environ.has_key('OPENSHIFT_DB_HOST'):
-        DB_HOST = os.environ['OPENSHIFT_DB_HOST']
-    if os.environ.has_key('OPENSHIFT_DB_PORT'):
-        DB_PORT = os.environ['OPENSHIFT_DB_PORT']
-
 
     if ON_OPENSHIFT:
+        print("USING OPENSHIFT DB")
         # os.environ['OPENSHIFT_DB_*'] variables can be used with databases created
         # with rhc app cartridge add (see /README in this git repo)
-        DATABASES = {
-            'default': {
+        return {
                 'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-                'NAME': DB_NAME,               # Or path to database file if using sqlite3.
-                'USER': DB_USER,               # Not used with sqlite3.
-                'PASSWORD': DB_PASSWD,         # Not used with sqlite3.
-                'HOST': DB_HOST,               # Set to empty string for localhost. Not used with sqlite3.
-                'PORT': DB_PORT,               # Set to empty string for default. Not used with sqlite3.
-            }
+                'NAME': os.path.join(DATA_DIR, 'appdb')
         }
     else:
+        print("USING LOCAL DB")
     # FOR LOCAL SERVER USE
-        DATABASES = {
-            'default': {
+        return {
                 'ENGINE': 'django.db.backends.mysql',
-                'NAME': 'test_db',
+                'NAME   ': 'test_db',
                 'USER': 'test',
                 'PASSWORD': 'SIA_passw0rd',
                 'HOST': '',
                 'PORT': '',
             }
-        }
