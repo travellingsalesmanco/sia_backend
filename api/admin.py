2cfcf9569219e10fec7df704c2506d0adaaf2139
tomforge <fooguowei3@gmail.com>
2017-10-24 21:47:58 +0800

    another bug fix

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib import admin
+from django.contrib.auth.admin import admin
 from django.contrib.auth.models import User
 
 from .models import Profile
