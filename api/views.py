7c717d4a6e83d8d525f269063fb4df5861169d62
johnphua <phuajohn0@gmail.com>
2017-10-27 03:56:45 +0800

    Resolved timestamp bug fix

--- api/views.py
+++ api/views.py
@@ -117,6 +117,7 @@ class CreateOrUpdateDefect(APIView):
             serializer.save()
             if defect.closed == True:
                 defect.dateResolved = timezone.now
+                defect.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
