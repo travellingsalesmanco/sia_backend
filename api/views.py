6dbfbdd774087a7722b10e889e6d45f05dfe43c1
johnphua <phuajohn0@gmail.com>
2017-10-26 15:53:03 +0800

    Bug fix: added import statement in views

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from api import models as m
 from api import serializers as s
